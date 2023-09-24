7754e24e21a9e2dc9f90ffd452fbf1feda7101a9
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2023-09-24 14:58:09 -0400

    added dynamic layout

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,9 @@
+const CoursesPage = () => {
+    return ( 
+        <div>
+            Coures page
+        </div>
+     );
+}
+ 
+export default CoursesPage;
--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
     return(
+        <Link href={"/"}>
         <Image
         height={130}
         width={130}
         alt="logo"
-        src="/logoipsum-248.svg" />
+        src="/logoipsum-245.svg" />
+        </Link>
+
     )
 }
