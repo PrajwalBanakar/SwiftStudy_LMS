9734a53ecde400dc3954160cb3c94c7334ebb15c
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2023-10-10 20:39:06 -0400

    added course title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,20 +3,20 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
 
 const courseIdPage = async (
     {
         params
-    } :
-    {
-      params:  {courseId: string} 
-    }
+    }:
+        {
+            params: { courseId: string }
+        }
 ) => {
 
     const { userId } = auth();
 
-    if(!userId)
-    {
+    if (!userId) {
         return redirect("/");
     }
 
@@ -28,8 +28,7 @@ const courseIdPage = async (
         }
     )
 
-    if(!course)
-    {
+    if (!course) {
         return redirect("/");
     }
 
@@ -47,30 +46,31 @@ const courseIdPage = async (
 
     const completetionText = `(${completedFields}/${totalFields})`
 
-    return ( 
+    return (
         <div className="p-6">
             <div className="flex items-center justify-between">
-            <div className="flex flex-col gap-y-2">
-            <h1 className="text-2xl font-medium">
-                Course setup
-            </h1>
-            <span className="text-sm text-slate-700">
-                Complete all fields {completetionText}
-            </span>
-            </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-            <div>
-                <div className="flex items-center gap-x-2">
-                    <IconBadge icon={LayoutDashboard}/>
-                <h2 className="text-xl">
-                    Customize your course
-                </h2>
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course setup
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete all fields {completetionText}
+                    </span>
                 </div>
             </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard} />
+                        <h2 className="text-xl">
+                            Customize your course
+                        </h2>
+                    </div>
+                    <TitleForm initialData={course} courseId={course.id}/>
+                </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default courseIdPage;
