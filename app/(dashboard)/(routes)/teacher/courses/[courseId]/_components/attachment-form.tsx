19ee003ff7657e88144cdbcc43314b43aead81ee
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2023-12-10 23:46:22 -0500

    add chapter creation API and modified schema

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 
 
 import { Button } from "@/components/ui/button";
-import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, X } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Attachment, Course } from "@prisma/client";
@@ -28,6 +28,7 @@ export const AttachmentForm = ({
 }: AttachmentFormProps) => {
 
     const [isEditing, setIsEditing] = useState(false);
+    const [deletingId, setDeletingId] = useState<string | null>(null);
 
     const toggleEdit = () => {
         setIsEditing((current) => !current);
@@ -44,6 +45,21 @@ export const AttachmentForm = ({
         }
         catch {
             toast.error("Unable to update course");
+        }
+    }
+
+    const onDelete = async (id: string) =>
+    {
+        try {
+            setDeletingId(id)
+            await axios.delete(`/api/courses/${courseId}/attachments/${id}`)
+            toast.success("Attachment deleted")
+            router.refresh()
+        } catch {
+            toast.error("Unable to delete")
+        }
+        finally {
+            setDeletingId(null)
         }
     }
 
@@ -67,6 +83,30 @@ export const AttachmentForm = ({
                 <>
                 {initialData.attachments.length === 0 && (
                     <p className="text-sm mt-2 text-slate-500 italic">No attachments yet</p>
+                )}
+                {initialData.attachments.length > 0 && (
+                    <div className="space-y-2">
+                        {initialData.attachments.map((attachment) => (
+                            <div key={attachment.id} className="flex items-center p-3 w-full
+                             bg-sky-100 border-sky-200 border
+                             text-sky-700 rounded-md">
+                                <File className="h-4 w-4 mr-2 flex-shrink-0"/>
+                                <p className="text-xs line-clamp-1">
+                                    {attachment.name}
+                                </p>
+                                {deletingId === attachment.id && (
+                                    <div>
+                                        <Loader2 className="h-4 w-4 animate-spin"/>
+                                    </div>
+                                )}
+                                {deletingId !== attachment.id && (
+                                    <button onClick={() => onDelete(attachment.id)} className="ml-auto hover:opacity-75 transition">
+                                        <X className="h-4 w-4"/>
+                                    </button>
+                                )}
+                            </div>
+                        ))}
+                    </div>
                 )}
                 </>
             )}
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 const courseIdPage = async (
     {
@@ -28,9 +29,15 @@ const courseIdPage = async (
     const course = await db.course.findUnique(
         {
             where: {
-                id: params.courseId
+                id: params.courseId,
+                userId
             },
             include: {
+                chapters: {
+                  orderBy: {
+                    position: "asc",
+                  },  
+                },
                 attachments:{
                     orderBy: {
                         createdAt: "desc",
@@ -57,7 +64,8 @@ const courseIdPage = async (
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished)
     ];
 
     const totalFields = requiredFields.length;
@@ -100,9 +108,7 @@ const courseIdPage = async (
                             <IconBadge icon={ListChecks}/>
                             <h2 className="text-xl">Course Chapters</h2>
                         </div>
-                        <div>
-                            TODO: Chapters
-                        </div>
+                        <ChaptersForm initialData={course} courseId={course.id}/>
                     </div>
                     <div>
                         <div className="flex items-center gap-x-2">
