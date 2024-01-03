64179957f1360fadbe344c3f3f6cfa35db736de2
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2024-01-03 16:30:45 -0500

    removed admin protection

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -6,12 +6,10 @@ import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { isTeacher } from "@/lib/teacher";
 
 import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
-  const { userId } = useAuth();
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
@@ -33,13 +31,13 @@ export const NavbarRoutes = () => {
               Exit
             </Button>
           </Link>
-        ) : isTeacher(userId) ? (
+        ): (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher mode
             </Button>
           </Link>
-        ) : null}
+        )}
         <UserButton
           afterSignOutUrl="/"
         />
